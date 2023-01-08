39aef1e0272e1f6fc6ff9082fc25a4db24326995
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2023-01-08 17:28:23 +0100

    Enable linting

--- typescript/src/drawio/model.ts
+++ typescript/src/drawio/model.ts
@@ -1,33 +1,32 @@
 export interface Point {
-    x: number,
-    y: number
+  x: number;
+  y: number;
 }
 
 export interface Size {
-    width: number,
-    height: number,
+  width: number;
+  height: number;
 }
 
-
 export interface Style {
-    name?: string,
-    rounded?: 0 | 1,
+  name?: string;
+  rounded?: 0 | 1;
 }
 
 export interface Element {
-    kind: 'object' | 'relation',
-    identifier: string,
-    label: string,
-    size?: Size,
-    position?: Point,
-    style: Style,
+  kind: "object" | "relation";
+  identifier: string;
+  label: string;
+  size?: Size;
+  position?: Point;
+  style: Style;
 }
 
 export interface Diagram {
-    identifier: string
-    name: string
+  identifier: string;
+  name: string;
 }
 
 export interface File {
-    diagrams: Diagram[]
+  diagrams: Diagram[];
 }
--- typescript/src/drawio/xml.ts
+++ typescript/src/drawio/xml.ts
@@ -1,95 +1,94 @@
-import * as Model from './model.js'
+import * as Model from "./model.js";
 
 interface MxPoint {
-    mxPoint: {
-        ":@": {
-            x: number,
-            y: number
-        }
-    }
+  mxPoint: {
+    ":@": {
+      x: number;
+      y: number;
+    };
+  };
 }
 
 interface MxArray {
-    Array: {
-        ":@": {
-            as: 'points'
-        },
-        points: MxPoint[]
-    }
+  Array: {
+    ":@": {
+      as: "points";
+    };
+    points: MxPoint[];
+  };
 }
 
 interface MxGeometry {
-    mxGeometry: {
-        ":@": {
-            height?: number,
-            width?: number,
-            relative?: 0 | 1,
-            as: 'geometry'
-        }
-    }
+  mxGeometry: {
+    ":@": {
+      height?: number;
+      width?: number;
+      relative?: 0 | 1;
+      as: "geometry";
+    };
+  };
 }
 
 interface MxCell {
-    mxCell: {
-        ":@": {
-            id?: string,
-            parent?: string,
-            value?: string,
-            style?: string,
-            vertex?: 0 | 1,
-        }
-        geometry?: MxGeometry
-    }
+  mxCell: {
+    ":@": {
+      id?: string;
+      parent?: string;
+      value?: string;
+      style?: string;
+      vertex?: 0 | 1;
+    };
+    geometry?: MxGeometry;
+  };
 }
 
 type MxElement = MxCell;
 
 function fromStyle(style: Model.Style) {
-    let property: keyof typeof style;
-    let st = [];
-    for (property in style) {
-        if (property === "name") {
-            st.push(`${style[property]}`)
-        } else {
-            st.push(`${property}=${style[property]}`)
-        }
+  let property: keyof typeof style;
+  const st = [];
+  for (property in style) {
+    if (property === "name") {
+      st.push(`${style[property]}`);
+    } else {
+      st.push(`${property}=${style[property]}`);
     }
-    return st.join(';')
+  }
+  return st.join(";");
 }
 
 function fromElement(elem: Model.Element): MxElement {
-    return {
-        mxCell: {
-            ":@": {
-                id: elem.identifier,
-                parent: "1",
-                value: elem.label ?? undefined,
-                style: fromStyle(elem.style),
-                vertex: 1
-            }
-        }
-    }
+  return {
+    mxCell: {
+      ":@": {
+        id: elem.identifier,
+        parent: "1",
+        value: elem.label ?? undefined,
+        style: fromStyle(elem.style),
+        vertex: 1,
+      },
+    },
+  };
 }
 
 function fromPoint(pt: Model.Point): MxPoint {
-    return {
-        mxPoint: {
-            ":@": {
-                x: pt.x,
-                y: pt.y
-            }
-        }
-    }
+  return {
+    mxPoint: {
+      ":@": {
+        x: pt.x,
+        y: pt.y,
+      },
+    },
+  };
 }
 
 function fromArray(pts: Model.Point[]): MxArray {
-    return {
-        Array: {
-
-            ":@": {
-                as: 'points'
-            },
-            points: pts.map(fromPoint)
-        }
-    }
+  return {
+    Array: {
+      ":@": {
+        as: "points",
+      },
+      points: pts.map(fromPoint),
+    },
+  };
 }
