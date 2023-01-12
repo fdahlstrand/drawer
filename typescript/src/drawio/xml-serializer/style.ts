8641239891414f4a22eb62f77aed9a3d8b53aa35
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2023-01-12 21:20:47 +0100

    Refactor style parsing

--- typescript/src/drawio/xml-serializer/style.ts
+++ typescript/src/drawio/xml-serializer/style.ts
@@ -1,7 +1,121 @@
 import * as Model from "../model.js";
 
-const Yes = Model.Option.Yes;
-const No = Model.Option.No;
+export class Style {
+  static stringify(style: Model.Style): string {
+    let property: keyof typeof style;
+    const st = [];
+    for (property in style) {
+      if (property in styleMapper) {
+        st.push(styleMapper[property](property, style[property]));
+      } else {
+        console.error(`Property ${property} is not implemented.`);
+      }
+    }
+    return st.join(";") + ";";
+  }
+
+  static parse(str: string): Model.Style {
+    const elems = str.split(";").filter((e) => e !== "");
+    let s: Model.Style = {};
+
+    elems.forEach((e) => {
+      const [property, value] = e.split("=") as [string, string];
+
+      if (value === undefined) {
+        s.name = property;
+      } else {
+        if (property in stringMapper) {
+          s = { ...s, ...stringMapper[property](property, value) };
+        } else {
+          console.warn(`Unknown property '${property}' ignored.`);
+        }
+      }
+    });
+    return s;
+  }
+}
+
+const styleMapper = (function () {
+  function optionMapper(key: string, value: Model.Option): string {
+    return `${key}=${value === Model.Option.Yes ? "1" : "0"}`;
+  }
+
+  function arrowStyleMapper(key: string, value: Model.ArrowStyle): string {
+    return `${key}=${arrowStyleMap[value]}`;
+  }
+
+  function numberMapper(key: string, value: number): string {
+    return `${key}=${value}`;
+  }
+
+  function stringMapper(key: string, value: string): string {
+    return `${key}=${value}`;
+  }
+
+  function styleNameMapper(key: string, value: string): string {
+    return `${value}`;
+  }
+
+  type StyleValue = number | string | Model.ArrowStyle;
+  return {
+    name: styleNameMapper,
+    html: optionMapper,
+    rounded: optionMapper,
+    whiteSpace: stringMapper,
+    startFill: optionMapper,
+    endFill: optionMapper,
+    strokeWidth: numberMapper,
+    startArrow: arrowStyleMapper,
+    endArrow: arrowStyleMapper,
+  } as {
+    [key in keyof Model.Style]: (key: string, value: StyleValue) => string;
+  };
+})();
+
+const stringMapper = (function () {
+  function numberMapper(property?: keyof Model.Style) {
+    return (key: string, value: string): Model.Style => ({
+      [property ?? key]: Number(value),
+    });
+  }
+
+  function stringMapper(property?: keyof Model.Style) {
+    return (key: string, value: string): Model.Style => ({
+      [property ?? key]: value,
+    });
+  }
+
+  function optionMapper(property?: keyof Model.Style) {
+    return (key: string, value: string): Model.Style => ({
+      [property ?? key]: value === "1" ? Model.Option.Yes : Model.Option.No,
+    });
+  }
+
+  function arrowStyleMapper(property?: keyof Model.Style) {
+    return (key: string, value: string): Model.Style => {
+      let k: keyof typeof Model.ArrowStyle;
+      for (k in arrowStyleMap) {
+        if (value === arrowStyleMap[k]) {
+          return {
+            [property ?? key]:
+              Model.ArrowStyle[k as keyof typeof Model.ArrowStyle],
+          };
+        }
+      }
+    };
+  }
+
+  return {
+    html: optionMapper(),
+    rounded: optionMapper(),
+    startFill: optionMapper(),
+    endFill: optionMapper(),
+    strokeWidth: numberMapper(),
+    startArrow: arrowStyleMapper(),
+    endArrow: arrowStyleMapper(),
+    whiteSpace: stringMapper(),
+  } as { [key: string]: (key: string, value: string) => Model.Style };
+})();
 
 const arrowStyleMap: { [key in keyof typeof Model.ArrowStyle]: string } = {
   None: "none",
@@ -31,102 +145,3 @@ const arrowStyleMap: { [key in keyof typeof Model.ArrowStyle]: string } = {
   ERzeroToMany: "ERzeroToMany",
   DoubleBlock: "doubleBlock",
 };
-
-function assertUnreachable(x: never): never {
-  x;
-  throw new Error("Didn't expect to get here");
-}
-
-function unsupportedStyle(property: string, value: string) {
-  console.warn(`Unexpected style ignored (${property}=${value})`);
-}
-
-export class Style {
-  static stringify(style: Model.Style): string {
-    let property: keyof typeof style;
-    const st = [];
-    for (property in style) {
-      switch (property) {
-        case "name":
-          st.push(`${style[property]}`);
-          break;
-        case "endFill":
-        case "html":
-        case "rounded":
-        case "startFill":
-          st.push(`${property}=${style[property] === Yes ? "1" : "0"}`);
-          break;
-        case "strokeWidth":
-          st.push(`${property}=${style[property]}`);
-          break;
-        case "whiteSpace":
-          st.push(`${property}=${style[property]}`);
-          break;
-        case "endArrow":
-        case "startArrow":
-          st.push(`${property}=${arrowStyleMap[style[property]]}`);
-          break;
-        default:
-          assertUnreachable(property);
-      }
-    }
-    return st.join(";") + ";";
-  }
-
-  static parse(str: string): Model.Style {
-    const elems = str.split(";").filter((e) => e !== "");
-    const s: Model.Style = {};
-    const option = (v: string): Model.Option => (v === "1" ? Yes : No);
-    elems.forEach((e) => {
-      const [property, value] = e.split("=") as [string, string];
-
-      if (value === undefined) {
-        s.name = property;
-      }
-
-      switch (property) {
-        case "endFill":
-        case "html":
-        case "rounded":
-        case "startFill":
-          s[property] = option(value);
-          break;
-        case "whiteSpace":
-          if (value === "wrap") {
-            s.whiteSpace = value;
-          }
-          break;
-        case "strokeWidth":
-          s[property] = Number(value);
-          break;
-        case "endArrow": {
-          let k: keyof typeof Model.ArrowStyle;
-          for (k in arrowStyleMap) {
-            if (value === arrowStyleMap[k]) {
-              s.endArrow =
-                Model.ArrowStyle[k as keyof typeof Model.ArrowStyle];
-            }
-          }
-          break;
-        }
-        case "startArrow": {
-          let k: keyof typeof Model.ArrowStyle;
-          for (k in arrowStyleMap) {
-            if (value === arrowStyleMap[k]) {
-              s.startArrow =
-                Model.ArrowStyle[k as keyof typeof Model.ArrowStyle];
-            }
-          }
-          break;
-        }
-        default:
-          if (value === undefined) {
-            s["name"] = value;
-          } else {
-            unsupportedStyle(property, value);
-          }
-      }
-    });
-    return s;
-  }
-}
