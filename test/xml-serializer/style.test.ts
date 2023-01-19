f626077cff66078dd2bc9f1ee00f3bb1adf5e07f
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2023-01-19 22:24:06 +0100

    Test numeric style properties

--- test/xml-serializer/style.test.ts
+++ test/xml-serializer/style.test.ts
@@ -2,6 +2,7 @@ import { No, Yes } from "../../src/drawio/drawio.js";
 import { Style } from "../../src/drawio/xml-serializer/style.js";
 
 const options = ["html", "rounded", "startFill", "endFill", "dashed"];
+const numbers = ["opacity", "strokeWidth", "perimeterSpacing"];
 
 describe("Stringify style", () => {
   test("empty style generates nothing", () => {
@@ -13,16 +14,38 @@ describe("Stringify style", () => {
     expect(Style.stringify({ name })).toBe(`${name};`);
   });
 
-  test("option enabled (Yes) sets value to 1", () => {
-    expect(Style.stringify({ html: Yes })).toBe("html=1;");
+  test("unknown property is not rendered", () => {
+    const consoleSpy = jest.spyOn(console, "error").mockImplementation(() => {
+      true;
+    });
+    expect(Style.stringify({ unknownProperty: Yes } as Style)).toBe("");
+    expect(consoleSpy).toBeCalled();
   });
 
-  test("option disabled (No) sets value to 0", () => {
-    expect(Style.stringify({ html: No })).toBe("html=0;");
+  describe("stringify option", () => {
+    test("option enabled (Yes) sets value to 1", () => {
+      expect(Style.stringify({ html: Yes })).toBe("html=1;");
+    });
+
+    test("option disabled (No) sets value to 0", () => {
+      expect(Style.stringify({ html: No })).toBe("html=0;");
+    });
+
+    test.each(options)("stringifies option '%s'", (opt) => {
+      expect(Style.stringify({ [opt]: Yes })).toBe(`${opt}=1;`);
+    });
   });
 
-  test.each(options)("stringifies option '%s'", (opt) => {
-    expect(Style.stringify({ [opt]: Yes })).toBe(`${opt}=1;`);
+  describe("stringify number values", () => {
+    test("stringify sets the value", () => {
+      const v = 42;
+      expect(Style.stringify({ opacity: v })).toBe(`opacity=${v};`);
+    });
+
+    test.each(numbers)("'%s' is a valid number property", (prop) => {
+      const v = 19;
+      expect(Style.stringify({ [prop]: v })).toBe(`${prop}=${v};`);
+    });
   });
 });
 
@@ -40,19 +63,41 @@ describe("Parse style", () => {
     expect(Style.parse(`${name};`)).toStrictEqual({ name });
   });
 
-  test("parse option with value 0, sets style to No", () => {
-    expect(Style.parse("html=0;")).toStrictEqual({ html: No });
+  test("unknown property is not parsed", () => {
+    const consoleSpy = jest.spyOn(console, "warn").mockImplementation(() => {
+      true;
+    });
+    expect(Style.parse("foo=1")).not.toContain("foo");
+    expect(consoleSpy).toBeCalled();
   });
 
-  test("parse option with value 99, sets style to No", () => {
-    expect(Style.parse("html=99;")).toStrictEqual({ html: No });
+  describe("parse options", () => {
+    test("parse option with value 0, sets style to No", () => {
+      expect(Style.parse("html=0;")).toStrictEqual({ html: No });
+    });
+
+    test("parse option with value 99, sets style to No", () => {
+      expect(Style.parse("html=99;")).toStrictEqual({ html: No });
+    });
+
+    test("parse option with value 1, sets style to Yes", () => {
+      expect(Style.parse("html=1;")).toStrictEqual({ html: Yes });
+    });
+
+    test.each(options)("parses option '%s'", (opt) => {
+      expect(Style.parse(`${opt}=1;`)).toStrictEqual({ [opt]: Yes });
+    });
   });
 
-  test("parse option with value 1, sets style to Yes", () => {
-    expect(Style.parse("html=1;")).toStrictEqual({ html: Yes });
-  });
+  describe("parse numbers", () => {
+    test("parses number property", () => {
+      const v = 47;
+      expect(Style.parse(`opacity=${v};`)).toStrictEqual({ opacity: v });
+    });
 
-  test.each(options)("parses option '%s'", (opt) => {
-    expect(Style.parse(`${opt}=1;`)).toStrictEqual({ [opt]: Yes });
+    test.each(numbers)("parses number for '%s'", (prop) => {
+      const v = 29;
+      expect(Style.parse(`${prop}=${v}`)).toStrictEqual({ [prop]: v });
+    });
   });
 });
